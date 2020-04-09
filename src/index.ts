6df44bff521c7beefbbaec52ba94eb3fa44ce559
Ibrahim <nibrahimb1993@gmail.com>
2020-04-09 13:25:59 +0300

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,24 @@
 // index.ts
 import { app, BrowserWindow } from 'electron'
-import express from 'express'
+import { Express } from 'express'
+import express = require('express')
 
-const expressApp = express()
-
-declare var __dirname: string
-let win
-expressApp.use(express.json())
-const server = expressApp.listen(4100, () => {
+const expressApp: Express = express()
+export const expressServer = expressApp.listen(4100, () => {
   console.log(`Server is listening on port ${4100}`)
 })
+declare var __dirname: string
+let win
+
 let createWindow = () => {
   win = new BrowserWindow({
     width: 450,
     height: 450,
   })
   win.loadURL(`file://${__dirname}/index.html`)
-  console.log('test')
   win.on('closed', () => {
     win = null
-    server.close()
+    expressServer.close()
   })
 }
 
